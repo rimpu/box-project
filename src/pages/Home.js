e43a5e5802cb7d84dc6676a037f8e3914c0258a3
aditibo <aditi.bose@citrix.com>
2020-10-19 21:28:08 +0530

    Added input box and handled onclick and onkeydown event

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {MainPageLayout} from "../components/MainPageLayout";
 
 export const Home = () => {
+
+  const [input, setInput] = useState("")
+
+  const onHandleChange = (event)=>{
+    setInput(event.target.value)
+  }
+
+  const onKeyDown = (event)=>{
+    if(event.keyCode === 13){
+      setInput(input);
+      fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
+      .then(res => res.json())
+      .then(result => console.log(result), setInput(""))
+    }
+  }
+
+  const onHandleClick =()=>{
+    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
+    .then(res => res.json())
+    .then(result => console.log(result), setInput(""))
+  }
+
   return (
     <MainPageLayout>
-      This is Home page.
+      <input type="text" value={input} onChange={onHandleChange} onKeyDown={onKeyDown}/>
+      <button type="click" onClick={onHandleClick}>Search</button>
     </MainPageLayout>
   )
 }
