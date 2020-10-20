4c2a7240233753e95eeb4e457ac1b07d0e1a4150
aditibo <aditi.bose@citrix.com>
2020-10-20 09:04:30 +0530

    Fetch API moved to a new location and added radio button input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,38 +6,77 @@ export const Home = () => {
 
   const [input, setInput] = useState("");
   const [result, setResult] = useState(null);
+  const [searchOption,setSearchOption] = useState("shows");
+  const isShowSearch = (searchOption === "shows");
 
-  const onHandleChange = (event)=>{
+
+  const onHandleChange = (event) => {
     setInput(event.target.value)
+  }
+
+  const searchItem = () =>{
+    getAllResults(`/search/${searchOption}?q=${input}`)
+    .then(result => setResult(result))
   }
 
   const onKeyDown = (event)=>{
     if(event.keyCode === 13){
       setInput(input);
-      getAllResults(`/search/shows?q=${input}`)
-      .then(result => setResult(result))
+      searchItem();
     }
   }
 
   const onHandleClick = () => {
-    getAllResults(`/search/shows?q=${input}`)
-    .then(result => setResult(result))
+    searchItem();
   }
 
   const showResults = () => {
     if(result && result.length ===0){
-      return <div>No results to show</div>
+      return <div>No results to show</div>;
     }
     else if(result && result.length > 0){
-    return <div>{result.map(elem=> <div key={elem.show.id}>{elem.show.name}</div>)}</div>
+    return result[0].show ? 
+           result.map(elem=> <div key={elem.show.id}>{elem.show.name}</div>) 
+           : result.map(elem=> <div key={elem.person.id}>{elem.person.name}</div>);
     }
-    return null
+    return null;
+  }
+
+  const onRadioChange = (event) =>{
+    setSearchOption(event.target.value);
   }
 
   return (
     <MainPageLayout>
-      <input type="text" value={input} onChange={onHandleChange} onKeyDown={onKeyDown}/>
-      <button type="click" onClick={onHandleClick}>Search</button>
+      <input 
+      type="text" 
+      value={input} 
+      onChange={onHandleChange} 
+      onKeyDown={onKeyDown}/>
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input 
+          id = "shows-search" 
+          type = "radio"
+          value = "shows"
+          checked = {isShowSearch}
+          onChange = {onRadioChange}/>
+        </label>
+        <label htmlFor="actors-search">
+          Actors
+          <input id="actors-search" 
+          type="radio"
+          value = "people"
+          checked = {!isShowSearch}
+          onChange = {onRadioChange}
+          />
+        </label>
+      </div>
+      <button 
+      type="click" 
+      onClick={onHandleClick}>
+      Search</button>
       {showResults()}
     </MainPageLayout>
   )
