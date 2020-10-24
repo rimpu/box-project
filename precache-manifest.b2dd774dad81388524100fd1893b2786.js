d7b246b466b9484bc90da86b1cb309defa6ef62e
aditibo <aditi.bose@citrix.com>
2020-10-24 18:02:44 +0530

    Updates

---
+++ precache-manifest.b2dd774dad81388524100fd1893b2786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af91e2a7255d013d56742c6657beeb24",
+    "url": "/box-project/index.html"
+  },
+  {
+    "revision": "dd8aaf76cf71183dbfe0",
+    "url": "/box-project/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "87083b97394fc22afaa4",
+    "url": "/box-project/static/js/2.e27aef88.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-project/static/js/2.e27aef88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd8aaf76cf71183dbfe0",
+    "url": "/box-project/static/js/main.caeff9ab.chunk.js"
+  },
+  {
+    "revision": "f17ada03af881f28c9a2",
+    "url": "/box-project/static/js/runtime-main.4e631308.js"
+  }
+]);
